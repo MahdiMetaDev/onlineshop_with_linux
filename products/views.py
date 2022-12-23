0ee73d4b5747aa19e41697c321050c7540c0eff7
Mahdi <mmf1344py@gmail.com>
2022-12-23 21:54:21 +0330

    add a queryset to ProductListView

--- products/views.py
+++ products/views.py
@@ -6,5 +6,9 @@ from .models import Product
 
 class ProductListView(generic.ListView):
     model = Product
+    
+    # will show active products
+    queryset = Product.objects.filter(active=True)
+    
     template_name = "products/product_list.html"
     context_object_name = "products"
