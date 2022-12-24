b93ab434f5799d89a2178ca58d742a1759d42ff1
Mahdi <mmf1344py@gmail.com>
2022-12-24 13:50:27 +0330

    add static files

--- config/settings.py
+++ config/settings.py
@@ -130,7 +130,10 @@ LOCALE_PATHS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+# Static files config
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
