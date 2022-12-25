1b8b5ec429aae1b4cd76497c058d7e0824952648
Mahdi <mmf1344py@gmail.com>
2022-12-25 12:57:10 +0330

    add Comment model & some translations

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
 
 class Product(models.Model):
@@ -17,4 +18,39 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse("product_detail", kwargs={"pk": self.pk})
+    
+
+class Comment(models.Model):
+    PRODUCT_STARS = (
+        ("1", _("Very Bad")),
+        ("2", _("Bad")),
+        ("3", _("Normal")),
+        ("4", _("Good")),
+        ("5", _("Perfect")),
+    )
+
+    author = models.ForeignKey(
+        get_user_model(),
+        verbose_name=_("author"),
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+
+    product = models.ForeignKey(
+        Product,
+        verbose_name=_("product"),
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+
+    text = models.TextField(_("text"),)
+    active = models.BooleanField(_("active"), default=True)
+    stars = models.CharField(_("stars"), max_length=10, choices=PRODUCT_STARS, blank=True)
+    
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self) -> str:
+        return f"{self.product.title}: {self.text}"
     
