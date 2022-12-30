9e65ab4f77681ebb5b7cf4649aede23fee57c182
Mahdi <mmf1344py@gmail.com>
2022-12-30 18:52:02 +0330

    add Cart class in cart app

---
+++ cart/cart.py
@@ -0,0 +1,91 @@
+from products.models import Product
+
+class Cart:
+
+    """ Initialize the cart """
+
+    def __init__(self, request) -> None:
+        self.request = request
+
+        self.session = request.session
+
+        cart = self.session.get("cart")
+
+        if not cart:
+            cart = self.session["cart"] = {}
+        
+        self.cart = cart
+
+
+    def add(self, product, quantity=1):
+
+        """ Add the specified product to the cart if it exists """
+
+        product_id = str(product.id)
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {"quantity": quantity}
+        else:
+            self.cart[product_id]["quantity"] += quantity
+        
+        self.save()
+
+
+    def remove(self, product):
+
+        """ Remove a product from the cart """
+
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+
+    def save(self):
+
+        """ Mark session as modified to save changes """
+
+        self.session.modified = True
+
+
+    def __iter__(self):
+        
+        """ With this we can loop over the Cart objects """
+        
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]["product_obj"] = product
+
+        for item in cart.values():
+            yield item
+
+
+    def __len__(self):
+        
+        """ Will give us how many types of products are there in 
+        the cart """
+
+        return len(self.cart.keys())
+    
+
+    def clear(self):
+
+        """ Clear the cart completely """
+
+        del self.session["cart"]
+        self.save()
+
+    
+    def get_total_price(self):
+
+        """ Calculate the total price for payment """
+
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+
+        return sum(product.price for product in products)
