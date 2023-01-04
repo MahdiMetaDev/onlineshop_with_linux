7d3230a5917be347d1a283ef142274742498ff0c
Mahdi <mmf1344py@gmail.com>
2023-01-04 07:44:58 +0330

    add templatetags for persian local app

---
+++ persian/templatetags/translation_tags.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def number_translate(value):
+
+    value = str(value)
+
+    english_to_persian_table = value.maketrans("0123456789", "۰۱۲۳۴۵۶۷۸۹")
+    result = value.translate(english_to_persian_table)
+
+    return result
