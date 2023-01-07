dd937756c528378d2af31f93d726c088c9b7fee2
Mahdi <mmf1344py@gmail.com>
2023-01-07 17:19:14 +0330

    create OrderItem model with order_notes field for Order model

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from jalali_date.admin import ModelAdminJalaliMixin
+
+from .models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    fields = ["order", "product", "quantity", "price"]
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    list_display = [
+        "user", "first_name", "last_name", "datetime_created", "is_paid",
+    ]
+
+    inlines = [
+        OrderItemInline,
+    ]
+    
--- orders/models.py
+++ orders/models.py
@@ -21,5 +21,45 @@ class Order(models.Model):
     phone_number = models.CharField(_("Phone_number"), max_length=15)
     address = models.CharField(_("address"), max_length=700)
 
+    order_notes = models.TextField(_("order_notes"), blank=True)
+
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("order")
+        verbose_name_plural = _("orders")
+
+    def __str__(self):
+        return f"Order {self.id}"
+    
+
+class OrderItem(models.Model):
+    
+    """ Include cart products data to pay """
+
+    order = models.ForeignKey(
+
+        Order,
+        on_delete=models.CASCADE,
+        related_name="items",
+        verbose_name=_("order"),
+
+    )
+
+    product = models.ForeignKey(
+
+        "products.Product",
+        on_delete=models.CASCADE,
+        verbose_name=_("product"),
+
+    )
+
+    quantity = models.PositiveIntegerField(_("quantity"), default=1)
+    price = models.PositiveIntegerField(_("price"))
+
+    class Meta:
+        verbose_name_plural = _("orderitems")
+
+    def __str__(self):
+        return f"OrderItem {self.id}: {self.product} * {self.quantity} (price:{self.price})"
