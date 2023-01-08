138e53787c8a263a151c8daa48dd8cc44cd93634
Mahdi <mmf1344py@gmail.com>
2023-01-08 12:22:37 +0330

    create objects of OrderItem for a special Order

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.utils.translation import gettext as _
+from django.contrib import messages
 
+from .models import OrderItem
 from .forms import OrderForm
+from cart.cart import Cart
 
 
 @login_required
@@ -10,6 +14,14 @@ def order_create_view(request):
     """ Users who have logged in can submit for the OrderForm """
 
     order_form = OrderForm()
+    cart = Cart(request)
+
+    if len(cart) == 0:
+        messages.warning(
+            request,
+            _("You can not proceed to checkout page because your cart is empty!"),
+        )
+        return redirect("product_list")
 
     if request.method == "POST":
         order_form = OrderForm(request.POST)
@@ -18,6 +30,26 @@ def order_create_view(request):
             order_obj = order_form.save(commit=False)
             order_obj.user = request.user
             order_obj.save()
+
+            for item in cart:
+                product = item["product_obj"]
+                OrderItem.objects.create(
+                    order=order_obj,
+                    product=product,
+                    quantity=item["quantity"],
+                    price=product.price,
+                )
+
+            cart.clear()
+
+            request.user.first_name = order_obj.first_name
+            request.user.last_name = order_obj.last_name
+            request.user.save()
+
+            messages.success(
+                request,
+                _("your order has successfully registered!"),
+            )
 
     return render(request, "orders/order_create.html", context={
         "form": order_form,
